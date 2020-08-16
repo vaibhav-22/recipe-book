3f04ac14f27512de4739cf8dfc661dab00f04a77
vaibhav bhimani <vaibhavbhimani2212@gmail.com>
2020-08-16 14:06:04 +0530

    Added navigation for recipe and shopping list section - Add functionality to load selected recipe in detail component. - Add functionality to add new ingredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'receipe-book';
+  featureLoaded ='recipe'; 
+
+  onNavigate(feature: string) {
+    this.featureLoaded = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [new Recipe('Test Recipe', 'This is test description','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
-  new Recipe('Test Recipe', 'This is test description','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')];
+  new Recipe('Test Recipe2', 'This is test2 description','https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg')];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectRecipeItem(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
